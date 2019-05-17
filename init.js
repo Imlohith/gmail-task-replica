68e87d7800324707c3664aac980d1fb82f72e7b0
lohith <viperlohith121@gmail.com>
2019-05-17 12:57:16 +0530

    added firebase

--- init.js
+++ init.js
@@ -1,4 +1,38 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-  });
+
+const TaskList = document.querySelector('#tasks');
+const form = document.querySelector('#add-task')
+const coverImage = document.querySelector('#cover-image')
+
+function renderTasks(doc) {
+   let li = document.createElement('li');
+   let title = document.createElement('span');
+
+   li.setAttribute('doc-id', doc.id)
+   title.textContent = doc.data().title;
+
+   li.appendChild(title)
+
+   TaskList.appendChild(li)
+}
+
+function displayCover(doc) {
+  if(doc) {
+      coverImage.style.display = 'none'
+  }
+}
+
+db.collection('tasks').get().then((snapShot) => {
+     snapShot.docs.forEach(doc => {
+         renderTasks(doc)
+         displayCover(doc)
+     });
+})
+
+//saving data
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('tasks').add({
+      title: form.title.value
+    })
+    form.title.value = '';
+})
